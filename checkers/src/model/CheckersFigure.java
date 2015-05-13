d5d90300755579b2c906a715fc1b7d74c98719df
HoreszB <alfred523@hotmail.com>
2015-05-13 23:35:57 +0200

    Kiegészítettem, egy függvénnyel, ami kiírja hova mozaghat a figurával
    Signed-off-by: HoreszB <alfred523@hotmail.com>

--- checkers/src/model/CheckersFigure.java
+++ checkers/src/model/CheckersFigure.java
@@ -5,4 +5,9 @@ public abstract class CheckersFigure extends Figure {
     public CheckersFigure(int posX, int posY, boolean dark) {
         super(posX, posY, dark);
     } 
+    //hova mozoghat a bábuval
+    public boolean canMoveTo(int x, int y) {
+        System.out.println(" chessfigure can move to" + x + ", " + y);
+        return false;
+    }
 }
